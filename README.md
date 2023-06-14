69e0584a757616eca53c0586282bb2b454affc51
djarami <djarami@gmail.com>
2023-06-13 22:20:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Student Server with NodeJS and Unit Test using Jest.io
+
+To run studentserver
+`npm install'
+'npm start'
+
+After running the server, you can view the swagger api-docs by loading this link on the browser:
+"http://localhost:5678/api-docs/" 
+
+To run unit tests
+`npm install --save-dev`
+`npm test`
---
+++ test/example.test.js
@@ -0,0 +1,86 @@
+const request = require('supertest')
+const {
+    app,
+    server
+} = require('../studentserver')
+
+//Test Fixtures
+const baseURL = "http://localhost:5678"
+const data = {
+    enrolled: true,
+    first_name: "James",
+    gpa: 3,
+    last_name: "Beam",
+}
+
+afterAll((done) => {
+    server.close(() => {
+        done();
+    });
+
+});
+
+describe('GET /students', () => {
+    var response
+
+    beforeAll(async () => {
+        response = await request(baseURL).get('/students');
+    });
+
+    it('should return a 200 status', async () => {
+        expect(response.status).toBe(200);
+
+    })
+
+    it('should return a list of students', async () => {
+        expect(response.body).toEqual({
+            students: [{
+                    enrolled: true,
+                    first_name: 'Jim',
+                    gpa: 3,
+                    last_name: 'Beam',
+                    record_id: 1677444963727
+                },
+                {
+                    enrolled: false,
+                    first_name: 'Janet',
+                    gpa: '5.0',
+                    last_name: 'Doe',
+                    record_id: '1677444956126'
+                },
+                {
+                    enrolled: true,
+                    first_name: 'John',
+                    gpa: 3,
+                    last_name: 'Doe',
+                    record_id: 1677444950300
+                }
+            ]
+        });
+
+    })
+});
+
+describe("POST /students", () => {
+    var response
+    beforeAll(async () => {
+        response = await request(baseURL).post('/students').send(data);
+    });
+
+
+    it('Should return 201', async () => {
+        expect(response.status).toBe(201);
+    });
+
+    it('Should have message, and record_id', async () => {
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('record_id');
+    });
+
+    afterAll(async () => {
+        // Clean up the newly created student
+        let student = response.body['record_id']
+        await request(baseURL).delete(`/students/${student}`)
+    });
+
+});
